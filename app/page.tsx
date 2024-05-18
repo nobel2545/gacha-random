51138d667192bd5a84030fda69d977b95d5b32c4
zeNOPE <nobel2545@gmail.com>
2024-05-18 23:08:27 +0700

    สมใจสมบูรณ์

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,90 @@
-import Image from "next/image";
+//ในหน้านี้มีโค้ดที่ทำให้ปุ่มสามารถ Random Page ได้ แต่ยังพบ Error เมื่อในไปใช้จริง
 
-export default function Home() {
+"use client"
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+const RandomPageButton: React.FC = () => {
+  const handleRandomPageClick = () => {
+    const pages = ['/salt', '/rare', '/legend'];
+    const randomIndex = Math.floor(Math.random() * pages.length);
+    const randomPage = pages[randomIndex];
+    window.location.href = randomPage;
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <img className="bg-cover absolute max-h-[740px]" src="rain.gif"/>
+
+      <div>
+        <img className="bg-cover absolute max-h-[740px]" src="room.PNG" />
+
+        <img className=" w-[300px] h-[300px] absolute top-[137px] right-[342px]" src="sohm.gif"/>
+
+        <img className=" w-[150px] h-[150px] absolute top-[255px] right-[160px]" src="deathflower.gif"/>
+
+        <div>
+          <div className="tag-tab font-thaibold absolute text-white p-3 pl-5 font-bold w-full " > ห้องฝ่ายบุคคลของโสมนัส / ตู้กาชา </div>
+          
+          <div className="flex absolute top-3 right-4 space-x-5">
+
+            <a href="/history">
+              <img className="w-[24px] h-[24px]" src="history.svg" />
+            </a>
+
+            <a href="/info">
+              <img className="w-[25px] h-[25px]" src="info-circle.svg" />
+            </a>
+            
+          </div>
+        </div>
+
+        <div className="mr-10 p-10 absolute flex space-x-10 flex-row bottom-0 right-0" >
+          <button onClick={handleRandomPageClick} className="tag-button hover:border-black hover:border-3 hover:bg-gray-400 hover:text-black" >Search x 1</button>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="ml-10 flex-row">
+
+        <div>
+
+        <div className="flex flex-col space-y-48 ">
+         <div>
+          <img className="w-[350px] absolute mt-10 p-10 cursor-pointer" src="folder_gacha.svg"/>
+          <div className="top-[115px] font-english text-black absolute left-[65px] p-10 text-white flex flex-col hover:text-black">
+           <h1 className="text-xl">Document For</h1>
+           <h2 className="text-5xl">Beginning</h2>
+          </div>
+         </div>
+
+        <div>
+         <img className="w-[350px] absolute mt-10 p-10 cursor-pointer" src="folder_gacha.svg"/>
+          <div className="top-[310px] font-english text-black absolute left-[65px] p-10 text-white flex flex-col hover:text-black">
+           <h1 className="text-xl">Time For</h1>
+           <h2 className="text-5xl">Revenge</h2>
+          </div>
+        </div>
+
+        <div>
+          <img className="w-[350px] absolute mt-10 p-10 cursor-pointer" src="folder_gacha.svg"/>
+          <div className="top-[500px] font-english text-black absolute left-[65px] p-10 text-white flex flex-col hover:text-black">
+            <h1 className="text-xl">Soul Pathfinder</h1>
+            <h2 className="text-5xl">Fire Series</h2>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          
+          </div>
+        </div>
+          
+      
+    </div>
+    
   );
-}
+};
+const App: React.FC = () => {
+  return (
+    <Router>
+      <RandomPageButton />
+    </Router>
+  );
+};
+export default App;
